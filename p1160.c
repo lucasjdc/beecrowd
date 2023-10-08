0f2d7db1e22023ff86c6880b9c4719a0900713b3
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-10-08 20:27:07 -0300

    Crescimento Populacional

---
+++ p1160.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int casos = 0; // número de casos de teste
+    int pa = 0; // população da cidade A
+    int pb = 0; // população da cidade B
+    float g1 = 0.0; // crescimento populacional da cidade A
+    float g2 = 0.0; // crescimento populacional da cidade B
+    int anos = 102;
+    
+    scanf("%d", &casos);
+    for (int i = 0; i < casos; i++) {
+        scanf("%d %d %f %f", &pa, &pb, &g1, &g2);
+        g1 /= 100;
+        g2 /= 100;
+
+        //while (getchar() != '\n'); // código para limpar o buffer de entrada
+
+        for (int j = 1; j < anos; j++) {
+            pa = pa + (pa * g1);
+            pb = pb + (pb * g2);
+            if  (pb < pa && j < 101) {
+                printf("%d anos.\n", j);
+                break;
+            }
+            if ( j > 100) {
+                printf("Mais de 1 seculo.\n");
+                break;
+            }
+        }
+    }
+}
